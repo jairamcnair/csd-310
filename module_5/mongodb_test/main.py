76b28516ae6edad7f42364aa11130fb92eaf53b6
jairamcnair <jaimcnair10@gmail.com>
2022-01-27 15:11:49 -0500

    git push

---
+++ module_5/mongodb_test/main.py
@@ -0,0 +1,8 @@
+import pymongo
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.vlpot.mongodb.net/pytech"
+
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names)
